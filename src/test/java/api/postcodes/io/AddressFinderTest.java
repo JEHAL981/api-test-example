f58d4b2b6de567c6789658dac1b7333c4a5e7fa7
JamesHalls <jameshalls@JamesHallss-MacBook-Pro.local>
2020-06-27 17:45:43 +0100

    updated files and deleted.

--- src/test/java/api/postcodes/io/AddressFinderTest.java
+++ src/test/java/api/postcodes/io/AddressFinderTest.java
@@ -1,5 +1,8 @@
 package api.postcodes.io;
 
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import io.restassured.response.ValidatableResponse;
 import org.junit.Test;
 
 import static io.restassured.RestAssured.*;
@@ -7,39 +10,51 @@ import static org.hamcrest.Matchers.*;
 
 public class AddressFinderTest {
 
+    public AddressFinderTest() {
+        RestAssured.baseURI = URLConfiguration.OPEN_URL_POSTCODE;
+    }
+
     @Test
     public void ShouldRetrievePostcode() {
-        given().
-                when().
-                get("http://api.postcodes.io/postcodes/SW1A 1AA").
-                then().
-                statusCode(200).
-                assertThat().
-                body("result.postcode", equalTo("SW1A 1AA"));
+        ValidatableResponse response =
+                given().
+                        contentType("application/json").
+                        when().
+                        get("postcodes/SW1A 1AA").
+                        then().
+                        statusCode(200).
+                        assertThat().
+                        body("result.postcode", equalTo("SW1A 1AA"));
 
 
     }
 
     @Test
     public void ShouldReturnInvalidMessageWhenNoPostCodeIsProvided() {
-        given().
-                when().
-                get("http://api.postcodes.io/postcodes/").
-                then().
-                statusCode(400).
-                assertThat().
-                body("error", equalTo("No postcode query submitted." +
-                        " Remember to include query parameter"));
+        ValidatableResponse response =
+                given().
+                        contentType("application/json").
+                        when().
+                        get("http://api.postcodes.io/postcodes/").
+                        then().
+                        statusCode(400).
+                        assertThat().
+                        body("error", equalTo("No postcode query submitted." +
+                                " Remember to include query parameter"));
     }
 
     @Test
     public void ShouldReturnInvalidPostcodeErrorMessage() {
-        given().
-                when().
-                get("http://api.postcodes.io/postcodes/123AB").
-                then().
-                statusCode(404).
-                assertThat().
-                body("error", equalTo("Invalid postcode"));
+        ValidatableResponse response =
+                given().
+                        contentType("application/json").
+                        when().
+                        get("http://api.postcodes.io/postcodes/123AB").
+                        then().
+                        statusCode(404).
+                        assertThat().
+                        body("error", equalTo("Invalid postcode"));
+
+
     }
 }
---
+++ src/test/java/api/postcodes/io/URLConfiguration.java
@@ -0,0 +1,6 @@
+package api.postcodes.io;
+
+public abstract class URLConfiguration {
+
+    public static final String OPEN_URL_POSTCODE = "http://api.postcodes.io/";
+}
